606dfdddc48856ef7edc1453062de04ac3695ae7
vaughes <11031393+vaughes@users.noreply.github.com>
2024-12-16 11:04:29 +0000

    Add string to cleck event

---
+++ app.js
@@ -0,0 +1,7 @@
+// add an X string to square 1 via a click event
+
+const square = document.getElementById("square_1");
+
+square.addEventListener("click", () => {
+  let x = square_1.append("X");
+});
