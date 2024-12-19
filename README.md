b740982842e070fd229c9db14e328b9d87ef455f
vaughes <11031393+vaughes@users.noreply.github.com>
2024-12-19 22:39:29 +0000

    Add game flow and play two squares

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ttt
+
+Noughts and crosses
+
+A player decides whether to play X or 0.
+A player can play in any square.
+Players take turns to play, e.g X is followed by 0.
+Once a square has been played in, it cannot be played in again.
+The winner is the first to achieve three in a row.
+If all tiles are filled without getting three in a row, it is a draw.
--- app.js
+++ app.js
@@ -1,54 +1,27 @@
-// Add a string to each square with a click event
+// Add an abortable event listener to square 1
+const controller_1 = new AbortController();
+const play1 = document.getElementById("square_1");
+play1.addEventListener("click", tileOne, { signal: controller_1.signal });
 
-const squareOne = document.getElementById("square_1");
-const squareTwo = document.getElementById("square_2");
-const squareThree = document.getElementById("square_3");
-const squareFour = document.getElementById("square_4");
-const squareFive = document.getElementById("square_5");
-const squareSix = document.getElementById("square_6");
-const squareSeven = document.getElementById("square_7");
-const squareEight = document.getElementById("square_8");
-const squareNine = document.getElementById("square_9");
-
-/*
-
-squareTwo.addEventListener("click", () => {
-  let x2 = square_2.append("X");
-  let n2 = square_2.append("0");
-});
-squareThree.addEventListener("click", () => {
-  let x3 = square_3.append("X");
-});
-squareFour.addEventListener("click", () => {
-  let x4 = square_4.append("X");
-});
-squareFive.addEventListener("click", () => {
-  let x5 = square_5.append("X");
-});
-squareSix.addEventListener("click", () => {
-  let x6 = square_6.append("X");
-});
-squareSeven.addEventListener("click", () => {
-  let x7 = square_7.append("X");
-});
-squareEight.addEventListener("click", () => {
-  let x8 = square_8.append("X");
-});
-squareNine.addEventListener("click", () => {
-  let x9 = square_9.append("X");
-});
-*/
-
-// Add an abortable event listener to grid
-const controller = new AbortController();
-const oneX = document.getElementById("grid");
-oneX.addEventListener("click", playOnce, { signal: controller.signal });
-
-// Function to allow only one go per tile
-function playOnce() {
+// Function to stop after a tile is filled
+function tileOne() {
   const square_1 = document.getElementById("square_1");
-  let x1 = square_1.append("X");
+  let x1 = square_1.append("0");
   if (square_1.firstChild.nodeValue === "X" || "0") {
-    controller.abort(); // remove listener after value reaches "X"
+    controller_1.abort(); // remove listener after value reaches "X" or "0"
+  }
+}
+
+// Add an abortable event listener to square 2
+const controller_2 = new AbortController();
+const play2 = document.getElementById("square_2");
+play2.addEventListener("click", tileTwo, { signal: controller_2.signal });
+
+// Function to stop after a tile is filled
+function tileTwo() {
+  const square_2 = document.getElementById("square_2");
+  let x2 = square_2.append("X");
+  if (square_2.firstChild.nodeValue === "X" || "0") {
+    controller_2.abort(); // remove listener after value reaches "X" or "0"
   }
 }
